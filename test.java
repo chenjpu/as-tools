a8a6541eb8e5e3fe241979362a9cef21c392806f
chenbing <chenjpu@gmail.com>
2018-01-20 15:16:40 +0800

    test1

---
+++ test.java
@@ -0,0 +1,10 @@
+final class MonoCurrentContext extends Mono<Context> implements Fuseable {
+
+	static final MonoCurrentContext INSTANCE = new MonoCurrentContext();
+
+	@SuppressWarnings("unchecked")
+	public void subscribe(CoreSubscriber<? super Context> actual) {
+		Context ctx = actual.currentContext();
+		actual.onSubscribe(Operators.scalarSubscription(actual, ctx));
+	}
+}
